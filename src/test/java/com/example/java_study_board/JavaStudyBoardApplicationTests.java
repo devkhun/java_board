09d38b438214ff607f08fec97e5e746bc054b63f
devkhun <devkhun409@gmail.com>
2024-03-19 10:55:55 +0900

    first commit

---
+++ src/test/java/com/example/java_study_board/JavaStudyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_study_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
