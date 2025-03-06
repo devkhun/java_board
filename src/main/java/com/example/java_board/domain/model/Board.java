81faf7c0d397a1a798200315af5a31076be66823
geehhyy <devkhun409@gmail.com>
2025-03-06 15:51:29 +0900

    UPDATE Board.java

--- src/main/java/com/example/java_board/domain/model/Board.java
+++ src/main/java/com/example/java_board/domain/model/Board.java
@@ -3,11 +3,13 @@ package com.example.java_board.domain.model;
 import java.util.Date;
 
 public class Board {
-    private Integer bno;
-    private String title;
-    private String content;
-    private String writer;
-    private String viewCnt;
-    private Date regDate;
-    private Date modDate;
+    private Integer boardId; // 게시글 ID
+    private String boardTitle; // 게시글 제목
+    private String boardContent; // 게시글 내용
+    private String boardStatus; // 게시글 상태 (예: active, deleted)
+    private String boardWriter; // 작성자
+    private String viewCnt; // 조회수
+    private Date createdAt; // 생성일
+    private Date updatedAt; // 수정일
+    private Date deletedAt; // 삭제일
 }
