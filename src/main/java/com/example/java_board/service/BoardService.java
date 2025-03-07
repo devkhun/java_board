6d02cccb9285353018b17cbf3045b629f145e1c1
geehhyy <devkhun409@gmail.com>
2025-03-07 15:23:27 +0900

    ADD RequiredArgsConstructor

--- src/main/java/com/example/java_board/service/BoardService.java
+++ src/main/java/com/example/java_board/service/BoardService.java
@@ -3,18 +3,14 @@ package com.example.java_board.service;
 
 import com.example.java_board.domain.entity.Board;
 import com.example.java_board.domain.repository.BoardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Component
+@RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
-
-    @Autowired
-    public BoardService(BoardRepository boardRepository) {
-        this.boardRepository = boardRepository;
-    }
 
     public Board createBoard(@RequestBody Board board) {
         return boardRepository.save(board);
