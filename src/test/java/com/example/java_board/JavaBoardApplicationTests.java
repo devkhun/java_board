60972c3c09f706437d64616758c11a34d868c159
geehhyy <devkhun409@gmail.com>
2025-03-07 15:07:21 +0900

    TEST database connection

--- src/test/java/com/example/java_board/JavaBoardApplicationTests.java
+++ src/test/java/com/example/java_board/JavaBoardApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.java_board;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JavaBoardApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void testDatabaseConnection() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            // 데이터베이스에 정상적으로 연결되었는지 확인
+            assertThat(connection).isNotNull();
+            assertThat(connection.isValid(2)).isTrue(); // 연결이 유효한지 확인 (2초)
+        }
+    }
 }
